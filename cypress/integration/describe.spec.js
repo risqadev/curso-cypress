d09e4c0996f74463aa9bdaa0a9a0554e68cf980f
Ricardo Camacho <rscamachodev@gmail.com>
2022-05-30 12:03:06 -0300

    describe and it

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+it('A external test...', () => {
+
+})
+
+describe('Should group test...', () => {
+    describe('Should group more specific test...', () => {
+        it('A specific test...', () => {
+            
+        })
+
+        it('A specific test 2...', () => {
+            
+        })
+    })
+
+    it('A internal test...', () => {
+
+    })
+})

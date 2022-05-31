7960a03fd0276e4e3f6fbf543c3a33590fc15da2
Ricardo Camacho <rscamachodev@gmail.com>
2022-05-31 11:56:01 -0300

    texts

---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+        cy.get('body')
+            .should('contain', 'Cuidado')
+        
+        cy.get('span')
+            .should('contain', 'Cuidado')
+        
+        cy.get('.facilAchar')
+            .should('contain', 'Cuidado')
+            .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+    })
+})

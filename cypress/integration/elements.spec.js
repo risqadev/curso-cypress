3147a6fcf8e90fd37b60140cfdc0c7b50fc9c818
Ricardo Camacho <rscamachodev@gmail.com>
2022-05-31 13:57:32 -0300

    links

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -14,4 +14,21 @@ describe('Work with basic elements', () => {
             .should('contain', 'Cuidado')
             .should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it.only('Links', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('a[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+        
+        cy.reload()
+
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+
+        // cy.get('a[href="#"]').click()
+        cy.contains('Voltar').click()   // outra forma de pegar
+
+        cy.get('#resultado').should('have.text', 'Voltou!')
+        
+    })
 })

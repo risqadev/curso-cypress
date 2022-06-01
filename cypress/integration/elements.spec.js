5a31504482b508d7a98028da8ebe9e1168535935
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-01 13:46:25 -0300

    combo box

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -67,7 +67,7 @@ describe('Work with basic elements', () => {
                 .should('not.be.checked')
         })
 
-        it.only('Checkbox', () => {
+        it('Checkbox', () => {
             cy.get('#formComidaPizza')
                 .click()
                 .should('be.checked')
@@ -80,5 +80,15 @@ describe('Work with basic elements', () => {
             
             cy.get('#formComidaVegetariana')
                 .should('be.checked')
+        })
+
+        it.only('Combo box', () => {
+            cy.get('[data-test=dataEscolaridade]')
+                .select('2o grau completo')
+                .should('have.value', '2graucomp')
+                
+            cy.get('[data-test=dataEscolaridade]')
+                .select('1graucomp')
+                .should('have.value', '1graucomp')
         })
 })

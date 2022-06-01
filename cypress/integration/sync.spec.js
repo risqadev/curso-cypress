4e9c597b88e9aa729752af28e01e3e24013c63d0
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-01 18:09:27 -0300

    Cypress is patient

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Esperas...', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Aguardar o elemento ficar disponível', () => {
+        cy.get('#novoCampo')
+            .should('not.exist')
+        cy.get('#buttonDelay')
+            .click()
+        cy.get('#novoCampo')
+            .should('not.exist')
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
+    })
+})

0f7968a7aaec326d8dece982a22cf7332bd53479
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-03 15:25:38 -0300

    should vs then

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -60,9 +60,34 @@ describe('Esperas...', () => {
             .should('contain', 'Item 2')
     })
 
-    it.only("click doesn't retry", () => {
+    it("click doesn't retry", () => {
         cy.get('#buttonCount')
             .click()
             .should('have.value', '111')
+    })
+
+    it.only("then vs. should", () => {
+        cy.get('#buttonList')
+            .click()
+        cy.get('#lista li span', {timeout: 7000})
+            .should('have.length', 2)
+        cy.get('#lista li span')
+            .then(elements => {
+                // console.log(elements)
+                expect(elements).have.length(2)
+                return 2    // com then é possível alterar o retorno
+            })
+            .then(previous => {
+                expect(previous).eq(2)
+            })
+        cy.get('#lista li span')
+            .should(elements => {
+                // console.log(elements)
+                expect(elements).have.length(2)
+                return 2    // com should não é possível alterar o retorno. O que foi recebido será passado adiante.
+            })
+            .then(previous => {
+                expect(previous).eq(2)
+            })
     })
 })

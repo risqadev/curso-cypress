c90002b410fa02c9383924235b136f51deb3552f
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-03 10:38:48 -0300

    printing page title

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () => {
-    it('Should visit a page and assert title', () => {
+    it.only('Should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         // const title = cy.title()
@@ -10,12 +10,19 @@ describe('Cypress basics', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
+
+        cy.title().then(title => {
+            console.log(title)
+        })
+        cy.title().should(title => {
+            console.log(title)
+        })
         
         // TODO imprimir log no console
         // TODO escrever o log em um campo de texto
     })
 
-    it.only('Should find and interact with an element', () => {
+    it('Should find and interact with an element', () => {
         const page = 'https://wcaquino.me/cypress/componentes.html'
         cy.visit(page)
 

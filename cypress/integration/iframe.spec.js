5d02d1296d65619ef8fa35507334fed2f7abb3e3
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-07 21:47:56 -0300

    iframes

---
+++ cypress/integration/iframe.spec.js
@@ -0,0 +1,27 @@
+/// <reference types='cypress' />
+
+describe('Work with iframes', () => {
+    it('iframe', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#frame1').then(iframe => {
+            const body = iframe.contents().find('body')
+
+            cy.wrap(body).find('#tfield')
+                .type('Texto digitado')
+                .should('have.value', 'Texto digitado')
+        })
+    })
+
+    it('testar o iframe no próprio escopo', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+
+        cy.on('window:alert', msg => expect(msg).to.be.equal('Click OK!'))
+
+        cy.get('#tfield')
+            .type('Texto digitado')
+            .should('have.value', 'Texto digitado')
+        
+        cy.get('#otherButton').click()
+    })
+})

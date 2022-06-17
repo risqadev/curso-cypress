119c3a253e1fb2f26cf3275b855556461ec6cf59
Ricardo Camacho <rscamachodev@gmail.com>
2022-06-17 16:22:22 -0300

    refactoring

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,7 +1,7 @@
 /// <reference types='cypress' />
 
-import locators from "../../support/locators"
-import fixtureConta from "../../fixtures/contaData.json"
+import locators from '../../support/locators'
+import fixConta from '../../fixtures/contaData.json'
 import '../../support/commandsContas'
 
 describe('Should test at a functional level', () => {
@@ -16,70 +16,80 @@ describe('Should test at a functional level', () => {
 
     it('Should create an account', () => {
         cy.acessarMenuConta()
-        cy.inserirConta(fixtureConta.contaNova.nome)
+        cy.inserirConta(fixConta.conta.nomeNova)
         
         cy.get(locators.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
 
     it('Should edit an account', () => {
         // cy.acessarMenuConta()
-        cy.get(locators.CONTAS.BTN_ALTERAR(fixtureConta.contaNova.nome))
+        cy.get(locators.CONTAS.BTN_ALTERAR(fixConta.conta.nomeNova))
             .click()
         cy.get(locators.CONTAS.NOME)
             .clear()
-            .type(fixtureConta.contaAlterada.nome)
+            .type(fixConta.conta.nomeAlterada)
         cy.get(locators.CONTAS.BTN_SALVAR).click()
         cy.get(locators.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
     })
 
     it('Should not create an account with same name', () => {
         // cy.acessarMenuConta()
-        cy.inserirConta(fixtureConta.contaAlterada.nome)
+        cy.inserirConta(fixConta.conta.nomeAlterada)
         cy.get(locators.MESSAGE).should('contain', 'code 400')
     })
 
     it('Should create a transaction', () => {
         cy.get(locators.MENU.MOVIMENTACAO).click()
-        cy.get(locators.MOVIMENTACAO.DESCRICAO).type(fixtureConta.movimentacao.descricao)
-        cy.get(locators.MOVIMENTACAO.VALOR).type(fixtureConta.movimentacao.valor)
-        cy.get(locators.MOVIMENTACAO.INTERESSADO).type(fixtureConta.movimentacao.interessado)
+        cy.get(locators.MOVIMENTACAO.DESCRICAO).type(fixConta.movimentacao.descricao)
+        cy.get(locators.MOVIMENTACAO.VALOR).type(fixConta.movimentacao.valor)
+        cy.get(locators.MOVIMENTACAO.INTERESSADO).type(fixConta.movimentacao.interessado)
+        cy.get(locators.MOVIMENTACAO.CONTA).select(fixConta.conta.nomeAlterada)
+        cy.get(locators.MOVIMENTACAO.STATUS).click()
         cy.get(locators.MOVIMENTACAO.BTN_SALVAR).click()
 
         cy.get(locators.EXTRATO.LINHAS).should('have.length', 7)
-        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixtureConta.movimentacao.descricao, fixtureConta.movimentacao.valor))
+        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixConta.movimentacao.descricao, fixConta.movimentacao.valor))
             .should('exist')
     })
 
+    it('Should get balance', () => {
+        cy.get(locators.MENU.HOME).click()
+        cy.get(locators.HOME.BUSCA_CONTA(fixConta.conta.nomeAlterada, '~ td'))
+            .should('contain', fixConta.movimentacao.valor.replace('.', ','))
+    })
+
     it('Should edit a transaction', () => {
-        // cy.get(locators.MENU.EXTRATO).click()
-        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixtureConta.movimentacao.descricao, fixtureConta.movimentacao.valor, 'i.fa-edit'))
+        cy.get(locators.MENU.EXTRATO).click()
+        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixConta.movimentacao.descricao, fixConta.movimentacao.valor, 'i.fa-edit'))
             .click()
-        cy.get(`${locators.MOVIMENTACAO.DESCRICAO}[value="${fixtureConta.movimentacao.descricao}"]`)
+        cy.get(`${locators.MOVIMENTACAO.DESCRICAO}[value="${fixConta.movimentacao.descricao}"]`)
             .clear()
-            .type(fixtureConta.movimentacao.descricaoAlteracao)
-        cy.get(`${locators.MOVIMENTACAO.VALOR}[value="${fixtureConta.movimentacao.valor}"]`)
+            .type(fixConta.movimentacao.descricaoAlteracao)
+        cy.get(`${locators.MOVIMENTACAO.VALOR}[value="${fixConta.movimentacao.valor}"]`)
             .clear()
-            .type(fixtureConta.movimentacao.valorAlteracao)
+            .type(fixConta.movimentacao.valorAlteracao)
         cy.get(locators.MOVIMENTACAO.BTN_SALVAR).click()
 
         cy.get(locators.EXTRATO.LINHAS).should('have.length', 7)
-        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixtureConta.movimentacao.descricaoAlteracao, fixtureConta.movimentacao.valorAlteracao))
+        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixConta.movimentacao.descricaoAlteracao, fixConta.movimentacao.valorAlteracao))
             .should('exist')
     })
 
     it('Should delete a transaction', () => {
         // cy.get(locators.MENU.EXTRATO).click()
-        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixtureConta.movimentacao.descricaoAlteracao, fixtureConta.movimentacao.valorAlteracao, 'i.fa-trash-alt'))
+        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixConta.movimentacao.descricaoAlteracao, fixConta.movimentacao.valorAlteracao, 'i.fa-trash-alt'))
             .click()
         cy.get(locators.MESSAGE).should('contain', 'Movimentação removida com sucesso!')
         cy.get(locators.EXTRATO.LINHAS).should('have.length', 6)
-        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixtureConta.movimentacao.descricaoAlteracao, fixtureConta.movimentacao.valorAlteracao))
+        cy.get(locators.EXTRATO.BUSCA_MOVIMENTACAO(fixConta.movimentacao.descricaoAlteracao, fixConta.movimentacao.valorAlteracao))
             .should('not.exist')
     })
 
     it('Should delete an account', () => {
         cy.acessarMenuConta()
-        cy.get(locators.CONTAS.BTN_REMOVER(fixtureConta.contaAlterada.nome)).click()
+        cy.get(locators.CONTAS.BTN_REMOVER(fixConta.conta.nomeAlterada)).click()
         cy.get(locators.MESSAGE).should('contain', 'Conta excluída com sucesso!')
+        cy.get(locators.CONTAS.BTN_ALTERAR(fixConta.conta.nomeAlterada))
+            .should('not.exist')
     })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -5,24 +5,31 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        HOME: '[data-test="menu-home"]',
         MOVIMENTACAO: '[data-test="menu-movimentacao"]',
         CONTAS: '[href="/contas"]',
         EXTRATO: '[data-test="menu-extrato"]',
         SETTINGS: '[data-test=menu-settings]',
         RESET: '[href="/reset"]'
     },
+    HOME: {
+        BUSCA_CONTA: (name, complement = '') =>
+            `tbody td:contains(${name}) ${complement}`
+    },
     CONTAS: {
         NOME: '[data-test=nome]',
         BTN_SALVAR: '.btn',
         BTN_ALTERAR: nomeConta => 
-            `table.table tr:contains(${nomeConta}) i.far.fa-edit`,
+            `table.table tr:contains(${nomeConta}) i.fa-edit`,
         BTN_REMOVER: nomeConta => 
-            `table.table tr:contains(${nomeConta}) i.far.fa-trash-alt`
+            `table.table tr:contains(${nomeConta}) i.fa-trash-alt`
     },
     MOVIMENTACAO: {
-        DESCRICAO: '[data-test="descricao"]',
-        VALOR: '[data-test="valor"]',
-        INTERESSADO: '[data-test="envolvido"]',
+        DESCRICAO: '[data-test=descricao]',
+        VALOR: '[data-test=valor]',
+        INTERESSADO: '[data-test=envolvido]',
+        CONTA: '[data-test="conta"]',
+        STATUS: '[data-test=status]',
         BTN_SALVAR: '.btn-primary'
     },
     EXTRATO: {
